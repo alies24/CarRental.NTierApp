fa5b73c3073c82a05772b40057ed4ff0772462c8
alies24 <ASUS@DESKTOP-6QFKKNT>
2022-03-28 12:53:39 +0300

    Add project files.

---
+++ Core/DataAccess/Concrete/EntityFrameworkCore/EfEntityReposBase.cs
@@ -0,0 +1,66 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DataAccess.Concrete
+{
+    public class EfEntityReposBase<TEntity, TContext> : IEntityRepository<TEntity>
+         where TEntity : class, IEntity, new()
+         where TContext : DbContext, new()
+    {
+        public void Add(TEntity entity)
+        {
+            using (TContext context = new TContext())
+            {
+                var addedEntity = context.Entry(entity);
+                addedEntity.State = EntityState.Added;
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(TEntity entity)
+        {
+            using (TContext context = new TContext())
+            {
+                var deleteEntity = context.Entry(entity);
+                deleteEntity.State = EntityState.Added;
+                context.SaveChanges();
+            }
+            
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> filter)
+        {
+            using (TContext context = new TContext())
+            {
+                var getEntity = context.Set<TEntity>().SingleOrDefault(filter);
+                return getEntity;
+            }
+        }
+
+        public List<TEntity> GetAll(Expression<Func<TEntity, bool>> filter = null)
+        {
+            using (TContext context = new TContext())
+            {
+                var getAll = filter == null ? context.Set<TEntity>().ToList() :
+                    context.Set<TEntity>().Where(filter).ToList();
+                return getAll;
+            }
+        }
+
+        public void Update(TEntity entity)
+        {
+            using (TContext context = new TContext())
+            {
+                var updateEntity = context.Entry(entity);
+                updateEntity.State = EntityState.Modified;
+                context.SaveChanges();
+            };
+        }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,30 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using System;
+
+namespace Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CarAdd();
+
+        }
+
+        private static void CarAdd()
+        {
+            Car car = new Car();
+            car.ColorId = 1;
+            car.BrandId = 1;
+            car.ModelYear = 2000;
+            car.DailyPrice = 150;
+            car.Description = "Deneme";
+
+            ICarService carService = new CarManager(new EfCarDal());
+            carService.Add(car);
+        }
+    }
+}
